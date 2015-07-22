f966a109466961d5300cc302f88bf6d6bcc26db2
Hsinpa <hsinpa@gmail.com>
2015-07-22 16:59:23 +0800

    'dfd'

--- Script/Game/RaycastDetector.cs
+++ Script/Game/RaycastDetector.cs
@@ -23,37 +23,38 @@ namespace Game {
 		}
 		
 		// Update is called once per frame
-//		void Update () {
-//			setSideHits();
-//			setVerticalHits();
-//		}
-
-		void OnDrawGizmos() {
+		void Update () {
 			setSideHits();
 			setVerticalHits();
 		}
 
+//		void OnDrawGizmos() {
+//			setSideHits();
+//			setVerticalHits();
+//		}
+
 		void setVerticalHits() {
 			float baseWidth = self.transform.position.x + (verticalGap*1.5f);
+			float y = (boxSize.y/2) + self.transform.position.y;
 			topHits.Clear();
 			bottomHits.Clear();
 
 			for (int i = 0; i < raycastLine; i++) {
-				Vector2 orginPos  = new Vector2(baseWidth, self.transform.position.y);
-				Vector2 topTargetPos = new Vector2(baseWidth, self.transform.position.y + (1*1.15f));
-				Vector2 bottomTargetPos = new Vector2(baseWidth, self.transform.position.y + (-1*1.15f));
+				Vector2 orginPos  = new Vector2(baseWidth, y);
+				Vector2 topTargetPos = new Vector2(baseWidth, y + (1.8f*1.15f));
+				Vector2 bottomTargetPos = new Vector2(baseWidth, y + (-1.8f*1.15f));
 
 				//Top
 				if (Physics2D.Linecast(orginPos, topTargetPos, ConstantVariable.platformLayer))
 					topHits.Add(Physics2D.Linecast(orginPos, topTargetPos, ConstantVariable.platformLayer));
-				Gizmos.color = Color.yellow;
-				Gizmos.DrawLine(orginPos, topTargetPos);
+				//Gizmos.color = Color.yellow;
+				//Gizmos.DrawLine(orginPos, topTargetPos);
 				//Bottom
 				if (Physics2D.Linecast(orginPos, bottomTargetPos, ConstantVariable.platformLayer)) 
 					bottomHits.Add(Physics2D.Linecast(orginPos, bottomTargetPos, ConstantVariable.platformLayer));
 
-				Gizmos.color = Color.green;
-				Gizmos.DrawLine(orginPos, bottomTargetPos);
+				//Gizmos.color = Color.green;
+				//Gizmos.DrawLine(orginPos, bottomTargetPos);
 
 				baseWidth -= verticalGap;
 			}
@@ -63,13 +64,15 @@ namespace Game {
 			int faceSide = 1;
 			sideHits.Clear();
 			//Gizmos.color = Color.blue;
-			float baseHeight = self.transform.position.y + (horizontalGap*1.5f);
+			float y = (boxSize.y/2) + self.transform.position.y;
+
+			float baseHeight = y + (horizontalGap*1.5f);
 			for (int i = 0; i < raycastLine; i++) {
 				Vector2 orginPos  = new Vector2(self.transform.position.x, baseHeight);
-				Vector2 targetPos = new Vector2(self.transform.position.x + (faceSide/1.5f), baseHeight);
+				Vector2 targetPos = new Vector2(self.transform.position.x + (faceSide/1f), baseHeight);
 				if (Physics2D.Linecast(orginPos, targetPos, ConstantVariable.platformLayer)) 
 					sideHits.Add( Physics2D.Linecast(orginPos, targetPos, ConstantVariable.platformLayer) );
-				Gizmos.DrawLine(orginPos, targetPos);
+				//Gizmos.DrawLine(orginPos, targetPos);
 
 				baseHeight -= horizontalGap;
 			}

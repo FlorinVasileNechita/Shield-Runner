ce5ef79db205dd90267da8e7adb3018721208aee
Hsinpa <hsinpa@gmail.com>
2015-08-26 17:03:25 +0800

    update

--- Script/Game/GameManager.cs
+++ Script/Game/GameManager.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
 
 namespace Game {
 	public class GameManager : MonoBehaviour {
@@ -8,7 +11,27 @@ namespace Game {
 		CameraManager camera;
 		GUI_MainManager guiManager;
 		public bool gameOn  = false;
+	
+		void Awake() {
+			PlayGamesPlatform.DebugLogEnabled = true;
+			PlayGamesPlatform.Activate();
+		}
+		
+		private void googlePlayLogin () 
+		{
+			if (PlayerPrefs.GetInt("user_id", 0) == 0) {
+				Social.localUser.Authenticate((bool success) => {
+					string UserName  = Social.localUser.userName; // UserName
+					string UserID      = Social.localUser.id; // UserID
+					PlayerPrefs.SetString("name", UserName);
+					PlayerPrefs.SetInt("user_id", UserID);
+					
+					Debugger.Log(UserID +", " +UserName);
+				});
+			}
+		}
 
+		
 		// Use this for initialization
 		void Start () {
 			guiManager = GameObject.Find("Canvas").GetComponent<GUI_MainManager>();
@@ -17,6 +40,8 @@ namespace Game {
 			player = GameObject.Find("Player").GetComponent<PlayerManager>();
 			camera = gameObject.GetComponent<CameraManager>();
 			PlayerReposition();
+
+			googlePlayLogin();
 		}
 
 		public void gameOver() {
